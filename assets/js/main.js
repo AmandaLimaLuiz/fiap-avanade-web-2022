88eb5e7b0967cc8991fe1e9ae5f21adc1d9b5361
AmandaLimaLuiz <amanda.de.lima.luiz@avanade.com>
2022-09-28 13:52:25 -0300

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//trabalhando com JS em um arquivo externo
+
+console.log(nome, 'chamando variavel criada com var');
+var nome = "FIAP";
+
+escreve("Exemplo de função nominal");
+
+//função nominal
+function escreve(titulo){
+    alert(titulo);
+}
